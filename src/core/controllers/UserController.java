2dc6fc19beb4f5fa0aebccc6871cd21d0c1b6277
krl0sconk <delcastillokrlos@gmail.com>
2026-05-16 01:25:52 -0500

    feat: UserController RegisterPatient

--- src/core/controllers/UserController.java
+++ src/core/controllers/UserController.java
@@ -7,6 +7,7 @@ package core.controllers;
 import core.controllers.utils.Response;
 import core.controllers.utils.Status;
 import core.models.storage.Storage;
+import core.models.user.Patient;
 import java.time.LocalDate;
 
 /**
@@ -17,11 +18,6 @@ public class UserController {
 
     //Validaciones
     private static boolean isValidId(long id) {
-        Storage storage = Storage.getInstance();
-        if (storage.getUserById(id) != null) {
-            // TODO: Cambiara a un try catch posiblemente.
-            return false;
-        }
         if (id < 0 || String.valueOf(Math.abs(id)).length() != 12) {
             return false;
         }
@@ -42,19 +38,27 @@ public class UserController {
         return true;
     }
 
-    private static boolean isValidDate(LocalDate birthdate) {
-        return false;
+    private static boolean isValidDate(String date) {
+        if (!date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            return false;
+        }
+        try {
+            LocalDate.parse(date); 
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     //Metodos
-    public static Response registerPatient(String firstname, String lastname, long id, boolean gender, LocalDate birthdate, String address, long phone, String email, String username, String password, String passwordConfirmation) {
+    public static Response registerPatient(String firstname, String lastname, long id, boolean gender, String birthdate, String address, long phone, String email, String username, String password, String passwordConfirmation) {
         try {
             Storage storage = Storage.getInstance();
 
             if (firstname.trim().equals("") || lastname.trim().equals("") || address.trim().equals("")) {
                 return new Response("All fields are required", Status.BAD_REQUEST);
             }
-            
+
             if (!isValidId(id)) {
                 return new Response("Invalid id.", Status.BAD_REQUEST);
             }
@@ -64,10 +68,21 @@ public class UserController {
             if (!isValidPhone(phone)) {
                 return new Response("Invalid phone.", Status.BAD_REQUEST);
             }
+            if (!isValidDate(birthdate)) {
+                return new Response("Invalid date.", Status.BAD_REQUEST);
+            }
+            
+            if (!password.equals(passwordConfirmation)) {
+                return new Response("Passwords do not match.", Status.BAD_REQUEST);
+            }
+            
+            if (!storage.addUser(new Patient(id, username, firstname, lastname, password, email, LocalDate.parse(birthdate), gender,phone, address))) {
+                return new Response("A Patient with that id already exists", Status.BAD_REQUEST);
+            }
+            return new Response("Patient registered succesfully.", Status.CREATED);
 
         } catch (Exception e) {
             return new Response("Unexpected error.", Status.INTERNAL_SERVER_ERROR);
         }
-        return null;
     }
 }
