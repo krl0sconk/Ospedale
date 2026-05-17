218fd0ca45564e5a3f345ec845c595635406adeb
krl0sconk <delcastillokrlos@gmail.com>
2026-05-17 15:33:18 -0500

    chore: added initialization to main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,31 @@
  */
 package main;
 
+import core.controllers.appointment.AppointmentController;
+import core.controllers.appointment.IAppointmentController;
+import core.controllers.hospitalization.HospitalizationController;
+import core.controllers.hospitalization.IHospitalizationController;
+import core.controllers.login.ILoginController;
+import core.controllers.login.LoginController;
+import core.controllers.usecases.HospitalizeFromAppointmentUseCase;
+import core.controllers.user.IUserController;
+import core.controllers.user.UserController;
+import core.models.storage.IStorage;
+import core.models.storage.Storage;
+
 /**
  *
  * @author krl0s
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+        IStorage storage = Storage.getInstance();
+
+        ILoginController loginController = new LoginController(storage);
+        IUserController userController = new UserController(storage);
+        IAppointmentController appointmentController = new AppointmentController(storage);
+        IHospitalizationController hospitalizationController = new HospitalizationController(storage);
+        HospitalizeFromAppointmentUseCase useCase = new HospitalizeFromAppointmentUseCase(appointmentController, hospitalizationController, storage);
     }
 }
